e576faca04b6de08bc48eda7b4de1b4ad49e0eaf
Ben Sturgis <ben.sturgis@rwth-aachen.de>
2021-05-02 23:47:00 +0200

    Deleted counters for function calls

--- src/zusatzaufgabe1/loesung.cpp
+++ src/zusatzaufgabe1/loesung.cpp
@@ -18,22 +18,19 @@ double detSimpsonIntegral(double a, double b, double fa, double fb, double fab2)
     return (b-a) * (1.0/6.0 * fa + 4.0/6.0 * fab2 + 1.0/6.0 * fb);
 }
 
-double detIntegralEqui(double a, double b, double epsilon, int &equiCounter,int intervalCounter) {
-    double h=(b-a)/intervalCounter;
-    double sum=0.5*(f(a)+f(b));
-    //equiCounter+=2;//hier f(a) and f(b) calculate f 2 times
-    for(int j=1;j<=intervalCounter-1;j++){
-        sum+=f(a+j*h);
-        //equiCounter++;//every loop calculate f 1 time
+double detIntegralEqui(double a, double b, int intervalCounter) {
+    double h = (b-a)/intervalCounter;
+    double sum = 0.5 * (f(a) + f(b));
+    for (int j = 1;j <= intervalCounter - 1; j++){
+        sum += f(a + j * h);
     }
-    sum*=h;
+    sum *= h;
     return sum;
 }
 
-double detIntegralAdap(double a, double b, double fa, double fb, double epsilon, int &adapCounter, int &intervalCounter,int curInterval) {
+double detIntegralAdap(double a, double b, double fa, double fb, double epsilon, int &intervalCounter, int curInterval) {
     double im, it;
     double fab2 = f((a+b)/2.0);
-    adapCounter += 3;
     im = detCenterIntegral(a, b, fab2);
     it = detTrapezeIntegral(a, b, fa, fb);
     if (abs(im-it) <= epsilon) {
@@ -41,10 +38,10 @@ double detIntegralAdap(double a, double b, double fa, double fb, double epsilon,
     } else {
         if(curInterval*2>intervalCounter){//I use your recursion to calculate the minimale Schrittweite, which is required for adaptive Algorithmus here,
         //so that I don't need to calculate all the functionsvalue and compare with epsilon again to get this Schrittweite in detIntegralEqui
-            intervalCounter*=2;
+            intervalCounter *= 2;
         }
-        return detIntegralAdap(a, (a+b)/2.0, fa, fab2, epsilon/2.0, adapCounter,intervalCounter,curInterval*2) 
-        + detIntegralAdap((a+b)/2, b, fab2, fb, epsilon/2.0, adapCounter, intervalCounter, curInterval*2);
+        return detIntegralAdap(a, (a+b)/2.0, fa, fab2, epsilon/2.0, intervalCounter, curInterval*2) 
+        + detIntegralAdap((a+b)/2, b, fab2, fb, epsilon/2.0, intervalCounter, curInterval*2);
     }
 }
 
@@ -53,7 +50,7 @@ int main(int argc, char *argv[]) {
     double a, b, epsilon;
     int ex_id;
     double integral;
-    int equiCounter = 0, adapCounter = 0,intervalCounter=1;
+    int intervalCounter = 1;
 
     cout << "Welches Beispiel soll gerechnet werden?" << endl;
     cin >> ex_id;
@@ -67,12 +64,9 @@ int main(int argc, char *argv[]) {
     getExample(ex_id, a, b, epsilon);
     double fa = f(a), fb = f(b);
 
-    integral = detIntegralAdap(a, b, fa, fb, epsilon, adapCounter, intervalCounter,1);
+    integral = detIntegralAdap(a, b, fa, fb, epsilon, intervalCounter, 1);
     checkSolution(integral);
     
-    integral = detIntegralEqui(a, b, epsilon, equiCounter,intervalCounter);
-    equiCounter=intervalCounter+1;////actually the function f calculates exactly intervalCounter times
+    integral = detIntegralEqui(a, b, intervalCounter);
     checkSolution(integral);
-
-    cout << "Bei der aequidistanten Unterteilung wurden " << equiCounter - adapCounter << " Funktionsauswertungen mehr benoetigt." << endl;
 }
