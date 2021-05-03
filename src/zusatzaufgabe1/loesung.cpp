92e49312501d15bf4d9a55e405cd8a2b61ae15bc
Ben Sturgis <ben.sturgis@rwth-aachen.de>
2021-05-03 12:25:40 +0200

    Fixed bug in equidistant partitioning

--- src/zusatzaufgabe1/loesung.cpp
+++ src/zusatzaufgabe1/loesung.cpp
@@ -18,9 +18,9 @@ double detSimpsonIntegral(double a, double b, double fa, double fb, double fab2)
     return (b-a) * (1.0/6.0 * fa + 4.0/6.0 * fab2 + 1.0/6.0 * fb);
 }
 
-double detIntegralEqui(double a, double b, int intervalCounter) {
+double detIntegralEqui(double a, double b, double fa, double fb, int intervalCounter) {
     double h = (b-a)/intervalCounter;
-    double sum = 0.5 * (f(a) + f(b));
+    double sum = 0.5 * (fa + fb);
     for (int j = 1;j <= intervalCounter - 1; j++){
         sum += f(a + j * h);
     }
@@ -50,12 +50,12 @@ int main(int argc, char *argv[]) {
     double a, b, epsilon;
     int ex_id;
     double integral;
-    int intervalCounter = 1;
+    int intervalCounter=1;
 
     cout << "Welches Beispiel soll gerechnet werden?" << endl;
     cin >> ex_id;
     while(cin.fail() || ex_id < 1 || ex_id > num_examples) {
-        cout << "Falsche Eingabe. Bitte natürliche Zahl zwischen 1 und " << num_examples << " eingeben." << endl;
+        cout << "Falsche Eingabe. Bitte natürliche Zahl zwischen 1 und " << num_examples << " eingeben.";
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(),'\n');
         cin >> ex_id;
@@ -67,6 +67,6 @@ int main(int argc, char *argv[]) {
     integral = detIntegralAdap(a, b, fa, fb, epsilon, intervalCounter, 1);
     checkSolution(integral);
     
-    integral = detIntegralEqui(a, b, intervalCounter);
+    integral = detIntegralEqui(a, b, fa, fb, intervalCounter);
     checkSolution(integral);
 }
