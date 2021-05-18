fe0df3224c06a6191362d1e090ab6b7da0fb9694
Ruizhang Zhou <ruizhangzhourick@gmail.com>
2021-05-18 12:26:49 +0200

    add determine symmetrisch but still lack the determine positiv definite

--- src/uebung3/main.cpp
+++ src/uebung3/main.cpp
@@ -9,9 +9,9 @@ using namespace std;
 bool istStrengDiagonalDominant(Sparse_Matrix &A){
     size_t n=A.getCols();
 
-    for(int row=1;row<=n;row++){
+    for(size_t row=1;row<=n;row++){
         double sumOfRow=0;
-        for(int col=1;col<=n;col++){
+        for(size_t col=1;col<=n;col++){
             if(row!=col){
                 sumOfRow+=abs(A.get(row,col));
             }
@@ -29,11 +29,11 @@ int gsv(Sparse_Matrix &A,const Vector &b,Vector &x0,const int &k_max,double &eps
     }
 
     size_t n=A.getCols();
-    Vector d(n);//use a vector to record all the elements of diagonal
+    Vector d= Vector(n);//use a vector to record all the elements of diagonal
     Sparse_Matrix originalA=Sparse_Matrix(A);//Kopierkonstuktor
     //but according to the last paragraph in paper, we can't use implement by create extra matrix or vector?
     
-    for(int row=1;row<=n;row++){
+    for(size_t row=1;row<=n;row++){
         
         d(row-1)=A.get(row,row);//as d is a str::vector, index begin from 0
         A.put(row,row,0);//set the dianoal to 0
@@ -48,8 +48,12 @@ int gsv(Sparse_Matrix &A,const Vector &b,Vector &x0,const int &k_max,double &eps
         if(iterCount>k_max){
             return 0;
         }else{
-            if((b-originalA*xk).norm2()>=eps){//here we need to use the zwischenspeichert
-            //but I haven't found out the way how to get b-A*xk from b-(AL+AR)*xk
+            Vector Residuenvektor=zwischenspeichert;
+            for(size_t i=1;i<=n;i++){
+                Residuenvektor(i)-=d(i)*xk(i);
+            }
+            if(Residuenvektor.norm2()>=eps){//here we need to use the zwischenspeichert
+            
             //Residuenvektor rk=(b-(AL+AR)*xk)-D*xk=zwischenspeichert-D*xk
                 xk=zwischenspeichert/d;
                 iterCount++;
@@ -61,15 +65,31 @@ int gsv(Sparse_Matrix &A,const Vector &b,Vector &x0,const int &k_max,double &eps
     checkSolution(xk,iterCount,0);//0 for GSV
     A=originalA;
     //Beim gsv darf A modifiziert werden, muss aber beim Verlassen der Funktion wieder wie beim Eingang besetzt sein.
-    return iterCount;
+    return iterCount;//here we resume that itercount can't be 0, 
+    //namely x0 from getExample isn't already the answer 
 }
 
 bool istSymPosDefinit(Sparse_Matrix &A){
-    //how to determine if a matrix is sysposdefinit?
+    //how to determind if a matrix is sysposdefinit?
+    size_t n=A.getCols();
+    //determine if it's symmetrisch
+    for(size_t row=1;row<=n;row++){
+        for(size_t col=row+1;col<=n;col++){
+            if(A.get(row,col)!=A.get(col,row)){
+                return false;
+            }
+        }
+    }
+
+    //determine if it's positiv symmetrisch
+    
 
 
 
 
+
+
+    return true;
 }
 
 int cg(const Sparse_Matrix &A,const Vector &b, Vector &x0, const int k_max, double &eps){
