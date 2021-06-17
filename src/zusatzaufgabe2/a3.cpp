13856c111b3721e3ae44d0a31e25c135e0848561
Ben Sturgis <ben.sturgis@rwth-aachen.de>
2021-06-17 16:39:48 +0200

    Implementation Miller-Rabin test

--- src/zusatzaufgabe2/a3.cpp
+++ src/zusatzaufgabe2/a3.cpp
@@ -2,10 +2,61 @@
 
 #include <cmath>
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 //------------------------------------------------------------------------------
 // bitte mit Leben fuellen:
 //------------------------------------------------------------------------------
+
+// Hilfsfunktionen
+
+void detBinaryRepresentation(lint nExp, vector<int>& x) {
+    if (nExp == 0) {
+        x.push_back(0);
+    } else {
+        while (nExp != 0) {
+            x.push_back(nExp % 2);
+            nExp = nExp / 2;
+        }
+    }
+}
+
+lint fastpow(lint nBase, lint nExp, lint nMod) {
+    lint aPowx = 1;
+    vector<int> x;
+    detBinaryRepresentation(nExp, x);
+    lint aPowTwoPowi = nBase % nMod;
+    if (x[0] == 1) {
+        aPowx = aPowTwoPowi;
+    }
+
+    for (int i = 1; i < (int) x.size(); i++) {
+        aPowTwoPowi = lint(powl(aPowTwoPowi, 2)) % nMod;
+        if (x[i] == 1) {
+            aPowx = (aPowx * aPowTwoPowi) % nMod;
+        }
+    }
+
+    return aPowx;
+}
+
+void getDS(lint n, lint &d, lint &s) {
+    lint r = 0;
+    lint twoPowr = (lint) powl(2,r);
+    
+    while ((n - 1) % twoPowr == 0) {
+        s = r;
+        r++;
+        twoPowr = twoPowr * 2;
+    }
+    
+    lint twoPows = twoPowr / 2;
+    d = (n-1) / twoPows;
+
+    return;
+}
 
 // Probedivision
 
@@ -27,28 +78,16 @@ lint fermatTest(lint n, lint a) {
 // Miller-Rabin
 
 lint millerRabinTest(lint n, lint a, lint d, lint r) {
-    return 0;
+    return fastpow(a, lint(powl(2, r)) * d, n);
 }
 
-// Hilfsfunktionen
 
-lint fastpow(lint nBase, lint nExp, lint nMod) {
-    return 0;
-}
-
-lint gcd(lint a, lint b) {
-    return 0;
-}
-
-void getDS(lint n, lint &d, lint &s) {
-    return;
-}
 
 //------------------------------------------------------------------------------
 // Hauptprogramm
 //------------------------------------------------------------------------------
 
-int main() {
+int main() { 
     Sieve sieve;
 
     sieve[0] = false;
@@ -62,15 +101,17 @@ int main() {
 
     checkSieve(sieve);
 
+    // PrimTest
+    
     for (unsigned int i = 1; i <= num_examples; i++) {
         lint test = getCandidate(DIVISION, i);
         checkSolution(DIVISION, i, trialDivision(test, sieve));
     }
-
-    // PrimTest
+ 
 
     // Fermat-Test
-    for(int i=1;i<=num_examples;i++){
+
+    for(int i=1;i<=(int) num_examples;i++){
         lint n=getCandidate(FERMAT,i);
         int y=1;
         
@@ -90,9 +131,38 @@ int main() {
         }
     }
 
-
-
     // Miller-Rabin-Test
+    
+    for (int i = 1; i <= (int) num_examples; i++) {
+        lint n = getCandidate(MILLER_RABIN, i);
+        lint f = 1;
+        lint d;
+        lint s;
+        lint y;
+
+        for (lint a = 1; a < maxPrimeNumber; a++) {
+            if (f == 0) {
+                break;
+            }
+            if (sieve[a] && n % a != 0) {
+                getDS(n, d, s);
+                if (fastpow(a, d, n) != 1) {
+                    for (lint r = 0; r < s; r++) {
+                        y = millerRabinTest(n, a, d, r);
+                        checkSolution(MILLER_RABIN, i, y, a, r);
+                        if (y == n - 1) {
+                            break;
+                        }
+                        if (r == s - 1) {
+                            f = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        checkSolution(MILLER_RABIN, i, f);
+    }
 
     return 0;
 }
