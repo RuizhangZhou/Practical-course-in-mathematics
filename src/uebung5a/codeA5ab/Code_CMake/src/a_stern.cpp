b54799c22e15288af33c02cb83ae166c1add8a97
Joshua Noeske <git@joshuanoeske.de>
2021-07-15 14:06:04 +0200

    Format code before merge.

--- src/uebung5a/codeA5ab/Code_CMake/src/a_stern.cpp
+++ src/uebung5a/codeA5ab/Code_CMake/src/a_stern.cpp
@@ -25,14 +25,14 @@ public:
     */
 protected:
     vector<NeighborT> adjacencyList;
-    vector<pair<double,double>> coordinates;
+    vector<pair<double, double>> coordinates;
 
 public:
     CoordinateGraph(int num_verts = 0)
-        : DistanceGraph(num_verts), adjacencyList(num_verts), coordinates(num_verts) {}
-    
+            : DistanceGraph(num_verts), adjacencyList(num_verts), coordinates(num_verts) {}
+
     virtual ~CoordinateGraph() {}
-    
+
     const NeighborT getNeighbors(VertexT v) const override {
         return adjacencyList[v];
     }
@@ -54,7 +54,7 @@ public:
         adjacencyList.resize(num_verts);
         coordinates.resize(num_verts);
     }
-    
+
     void addEdge(VertexT vertex, LocalEdgeT edge) {
         adjacencyList[vertex].push_back(edge);
     }
@@ -63,13 +63,13 @@ public:
         coordinates[vertex] = coord;
     }
 
-    friend std::istream& operator>>(std::istream&, CoordinateGraph&);
+    friend std::istream &operator>>(std::istream &, CoordinateGraph &);
 };
 
 class EucliGraph : public CoordinateGraph {
 public:
     EucliGraph(int num_verts = 0)
-        : CoordinateGraph(num_verts) {}
+            : CoordinateGraph(num_verts) {}
 
     virtual ~EucliGraph() {}
 
@@ -86,8 +86,8 @@ public:
 class CircEucliGraph : public EucliGraph {
 public:
     CircEucliGraph(int num_verts = 0)
-        : EucliGraph(num_verts) {}
-    
+            : EucliGraph(num_verts) {}
+
     ~CircEucliGraph() {
         for (size_t i = 0; i < adjacencyList.size(); i++) {
             adjacencyList[i].clear();
@@ -99,7 +99,7 @@ public:
         coordinates.clear();
         coordinates.shrink_to_fit();
     }
-    
+
     CostT estimatedCost(VertexT from, VertexT to) const override {
         double distance = min(euclideanDistance(from, to), min(cost(from, to), cost(to, from)));
         return distance;
@@ -109,8 +109,8 @@ public:
 class ShortDistEucliGraph : public EucliGraph {
 public:
     ShortDistEucliGraph(int num_verts = 0)
-        : EucliGraph(num_verts) {}
-    
+            : EucliGraph(num_verts) {}
+
     ~ShortDistEucliGraph() {
         for (size_t i = 0; i < adjacencyList.size(); i++) {
             adjacencyList[i].clear();
@@ -122,7 +122,7 @@ public:
         coordinates.clear();
         coordinates.shrink_to_fit();
     }
-    
+
     CostT estimatedCost(VertexT from, VertexT to) const override {
         return euclideanDistance(from, to);
     }
@@ -131,16 +131,16 @@ public:
 class LonLatCoordGraph : public CoordinateGraph {
 public:
     LonLatCoordGraph(int num_verts = 0)
-        : CoordinateGraph(num_verts) {}
-    
+            : CoordinateGraph(num_verts) {}
+
     virtual ~LonLatCoordGraph() {}
 
     double latLongToDist(VertexT from, VertexT to) const {
         double earthRadius = 6378.388;
-        double lon1 = coordinates[from].second * M_PI/180;
-        double lon2 = coordinates[to].second * M_PI/180;
-        double lat1 = coordinates[from].first * M_PI/180;
-        double lat2 = coordinates[to].first * M_PI/180;
+        double lon1 = coordinates[from].second * M_PI / 180;
+        double lon2 = coordinates[to].second * M_PI / 180;
+        double lat1 = coordinates[from].first * M_PI / 180;
+        double lat2 = coordinates[to].first * M_PI / 180;
         double dist = earthRadius * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1));
 
         return dist;
@@ -150,8 +150,8 @@ public:
 class LongDistCoordGraph : public LonLatCoordGraph {
 public:
     LongDistCoordGraph(int num_verts = 0)
-        : LonLatCoordGraph(num_verts) {}
-    
+            : LonLatCoordGraph(num_verts) {}
+
     ~LongDistCoordGraph() {
         for (size_t i = 0; i < adjacencyList.size(); i++) {
             adjacencyList[i].clear();
@@ -163,7 +163,7 @@ public:
         coordinates.clear();
         coordinates.shrink_to_fit();
     }
-    
+
     CostT estimatedCost(VertexT from, VertexT to) const override {
         return latLongToDist(from, to);
     }
@@ -172,8 +172,8 @@ public:
 class TimeCoordGraph : public LonLatCoordGraph {
 public:
     TimeCoordGraph(int num_verts = 0)
-        : LonLatCoordGraph(num_verts) {}
-    
+            : LonLatCoordGraph(num_verts) {}
+
     ~TimeCoordGraph() {
         for (size_t i = 0; i < adjacencyList.size(); i++) {
             adjacencyList[i].clear();
@@ -185,13 +185,13 @@ public:
         coordinates.clear();
         coordinates.shrink_to_fit();
     }
-    
-    CostT estimatedCost( VertexT from, VertexT to) const override {
-        return (latLongToDist(from, to)/200.0)*60.0;
+
+    CostT estimatedCost(VertexT from, VertexT to) const override {
+        return (latLongToDist(from, to) / 200.0) * 60.0;
     }
 };
 
-istream& operator>>(istream &s, CoordinateGraph &graph) {
+istream &operator>>(istream &s, CoordinateGraph &graph) {
     int num_verts;
     int num_edges;
     VertexT vertex;
@@ -204,7 +204,7 @@ istream& operator>>(istream &s, CoordinateGraph &graph) {
 
     s >> num_edges;
 
-    for (int i = 0; i < num_edges; i++){
+    for (int i = 0; i < num_edges; i++) {
         s >> vertex;
         s >> edge.first;
         s >> edge.second;
@@ -220,11 +220,11 @@ istream& operator>>(istream &s, CoordinateGraph &graph) {
     return s;
 }
 
-void Dijkstra(const DistanceGraph& g, GraphVisualizer& v, VertexT start, std::vector<CostT>& kostenZumStart) {
+void Dijkstra(const DistanceGraph &g, GraphVisualizer &v, VertexT start, std::vector<CostT> &kostenZumStart) {
     // ...
 }
 
-bool A_star(const DistanceGraph& g, GraphVisualizer& v, VertexT start, VertexT ziel, std::list<VertexT>& weg) {
+bool A_star(const DistanceGraph &g, GraphVisualizer &v, VertexT start, VertexT ziel, std::list<VertexT> &weg) {
     // ...
     vector<CostT> f_v;//f(v)=g(v)+h(v)
     vector<VertexT> vorgaenger;
@@ -232,35 +232,36 @@ bool A_star(const DistanceGraph& g, GraphVisualizer& v, VertexT start, VertexT z
     vector<VertexT> untersuchterKnoten;
 
     bekannteKnoten.push_back(start);
-    for(auto i=0;i<g.numVertices();i++){
+    for (auto i = 0; i < g.numVertices(); i++) {
         f_v.push_back(infty);
     }
-    f_v[start]=g.estimatedCost(start,ziel);
+    f_v[start] = g.estimatedCost(start, ziel);
 
-    while(!bekannteKnoten.empty()){
+    while (!bekannteKnoten.empty()) {
         //use the priority queue(make_heap) which mentioned in the script
         //here should compare the VertexT by f_v
 
-        make_heap(bekannteKnoten.begin(),bekannteKnoten.end(),greater<VertexT>());//smallest value at the front,but how to rewrite the comparator for VertexT here?
-        pop_heap(bekannteKnoten.begin(),bekannteKnoten.end(),greater<VertexT>());//smallest value move to the back
-        VertexT minVertexT=bekannteKnoten.back();//now curVertexT is the smallest one
-        if(minVertexT==ziel){
-            VertexT curV=ziel;
+        make_heap(bekannteKnoten.begin(), bekannteKnoten.end(),
+                  greater<VertexT>());//smallest value at the front,but how to rewrite the comparator for VertexT here?
+        pop_heap(bekannteKnoten.begin(), bekannteKnoten.end(), greater<VertexT>());//smallest value move to the back
+        VertexT minVertexT = bekannteKnoten.back();//now curVertexT is the smallest one
+        if (minVertexT == ziel) {
+            VertexT curV = ziel;
             weg.push_front(ziel);
-            while(curV!=start){
-                curV=vorgaenger[curV];
+            while (curV != start) {
+                curV = vorgaenger[curV];
                 weg.push_front(curV);
             }
             return true;
         }
         bekannteKnoten.pop_back();//remove the minVertexT
         untersuchterKnoten.push_back(minVertexT);
-        for (auto curE : g.getNeighbors(minVertexT)){
-            if(find(untersuchterKnoten.begin(),untersuchterKnoten.end(),curE.first)==untersuchterKnoten.end()){
-                CostT newf_v=f_v[minVertexT]+g.cost(minVertexT,curE.first)+g.estimatedCost(curE.first,ziel);
-                if(newf_v<f_v[curE.first]){
-                    vorgaenger[curE.first]=minVertexT;
-                    f_v[curE.first]=newf_v;
+        for (auto curE : g.getNeighbors(minVertexT)) {
+            if (find(untersuchterKnoten.begin(), untersuchterKnoten.end(), curE.first) == untersuchterKnoten.end()) {
+                CostT newf_v = f_v[minVertexT] + g.cost(minVertexT, curE.first) + g.estimatedCost(curE.first, ziel);
+                if (newf_v < f_v[curE.first]) {
+                    vorgaenger[curE.first] = minVertexT;
+                    f_v[curE.first] = newf_v;
                 }
             }
         }
@@ -268,8 +269,7 @@ bool A_star(const DistanceGraph& g, GraphVisualizer& v, VertexT start, VertexT z
     return false; // Kein Weg gefunden.
 }
 
-int main()
-{
+int main() {
     // Frage Beispielnummer vom User ab
     // Lade die zugehoerige Textdatei in einen Graphen
     // PruefeHeuristik
@@ -315,10 +315,10 @@ int main()
         cout << "Ungültige Beispielnummer." << endl;
         return -1;
     }
-    
+
     // Loese die in der Aufgabenstellung beschriebenen Probleme fuer die jeweilige Datei
     // PruefeDijkstra / PruefeWeg
-    
+
     return 0;
 }
  
