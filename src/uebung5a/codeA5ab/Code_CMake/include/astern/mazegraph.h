98c3b50cbebf09cb88ba02f3d88371f75503f7a9
Ben Sturgis <ben.sturgis@rwth-aachen.de>
2021-07-15 11:58:18 +0200

    Implementation of different graph classes

---
+++ src/uebung5a/codeA5ab/Code_CMake/include/astern/mazegraph.h
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#include "unit.h"
+
+using namespace std;
+using LocalEdgeT = std::pair<VertexT,CostT>;
+
+class MazeGraph : public DistanceGraph {
+    protected:
+        size_t height;
+        size_t width;
+        vector<CellType> nodes;
+        vector<NeighborT> adjacencyList;
+    
+    public:
+        MazeGraph(int num_verts = 0)
+            : DistanceGraph(num_verts), nodes(num_verts), adjacencyList(num_verts) {}
+
+        MazeGraph(vector<CellType> maze, int h, int w) {
+            width = w;
+            height = h;
+            vertexCount = width * height;
+            resisze(numVertices());
+            
+            for (size_t i = 0; i < numVertices(); i++) {
+                if (maze[i] == CellType::Start || maze[i] == CellType::Destination) {
+                    maze[i] = CellType::Ground;
+                }
+            }
+
+            nodes = maze;
+
+            calcAdjacencyList();
+        }
+        
+        ~MazeGraph() {
+            for (size_t i = 0; i < adjacencyList.size(); i++) {
+                adjacencyList[i].clear();
+                adjacencyList[i].shrink_to_fit();
+            }
+            adjacencyList.clear();
+            adjacencyList.shrink_to_fit();
+
+            nodes.clear();
+            nodes.shrink_to_fit();
+        }
+
+        const NeighborT getNeighbors(VertexT v) const override {
+            return adjacencyList[v];
+        }
+
+        CostT cost(VertexT from, VertexT to) const override {
+            if (from == to) {
+                return 0;
+            }
+            
+            for (LocalEdgeT edge : adjacencyList[from]) {
+                if (edge.first == to) {
+                    return 1;
+                }
+            }
+
+            return infty;
+        }
+
+        CostT estimatedCost(VertexT from, VertexT to) const override {
+            int heightFrom = from/width;
+            int heightTo = to/width;
+            int heightDiff = heightTo - heightFrom;
+
+            int widthFrom = from % width;
+            int widthTo = to % width;
+            int widthDiff = widthTo - widthFrom;
+
+            double dist = sqrt(pow(heightDiff, 2) + pow(widthDiff, 2));
+
+            return dist;
+        }
+
+        void resisze(int num_verts) {
+            vertexCount = num_verts;
+            nodes.resize(num_verts);
+            adjacencyList.resize(num_verts);
+        }
+
+        void addEdge(VertexT vertex, LocalEdgeT edge) {
+            adjacencyList[vertex].push_back(edge);
+        }
+
+        void calcAdjacencyList() {
+            pair<VertexT, CostT> edge;
+
+            for (size_t i = 0; i < numVertices(); i++) {
+                if (nodes[i] == CellType::Ground) {
+                    if (i % width > 0 && nodes[i - 1] == CellType::Ground) {
+                        edge.first = i - 1;
+                        edge.second = 1;
+                        addEdge(i, edge);
+                    }
+
+                    if (i % width < width - 1 && nodes[i + 1] == CellType::Ground) {
+                        edge.first = i + 1;
+                        edge.second = 1;
+                        addEdge(i, edge);
+                    }
+
+                    if (i >= width && nodes[i - width] == CellType::Ground) {
+                        edge.first = i - width;
+                        edge.second = 1;
+                        addEdge(i, edge);
+                    }
+
+                    if (i < numVertices() - width && nodes[i + width] == CellType::Ground) {
+                        edge.first = i + width;
+                        edge.second = 1;
+                        addEdge(i, edge);
+                    }
+                }
+            }
+        }
+
+        friend std::istream& operator>>(std::istream&, MazeGraph&);
+};
+
+istream& operator>>(istream &s, MazeGraph &graph) {
+    char currNode;
+    
+    s >> graph.height;
+    s >> graph.width;
+
+    graph.resisze(graph.height * graph.width);
+
+    for (size_t i = 0; i < graph.height * graph.width; i++) {
+        s >> currNode;
+
+        if (currNode == '#') {
+            graph.nodes[i] = CellType::Wall;
+        } else if (currNode == '.') {
+            graph.nodes[i] = CellType::Ground;
+        }
+    }
+
+    graph.calcAdjacencyList();
+
+    return s;
+     
+}
--- src/uebung5a/codeA5ab/Code_CMake/src/a_stern.cpp
+++ src/uebung5a/codeA5ab/Code_CMake/src/a_stern.cpp
@@ -1,6 +1,7 @@
 
 #include "astern/text_visualizer.h"
 #include "astern/unit.h"
+#include "astern/mazegraph.h"
 #include <vector>
 #include <algorithm>//make_heap
 #include <functional>//greater
@@ -10,56 +11,207 @@ using namespace std;
 
 // Ein Graph, der Koordinaten von Knoten speichert.
 class CoordinateGraph : public DistanceGraph {
-    map<EdgeT,CostT> costsMap;//I'm not sure, maybe use a map to memory all the costs of edges and costs?
+protected:
+    vector<NeighborT> adjacencyList;
+    vector<pair<double,double>> coordinates;
+
 public:
-    const NeighborT getNeighbors( VertexT v) const override;
+    CoordinateGraph(int num_verts = 0)
+        : DistanceGraph(num_verts), adjacencyList(num_verts), coordinates(num_verts) {}
     
-    CostT estimatedCost( VertexT from, VertexT to) const override;
+    virtual ~CoordinateGraph() {}
     
-    CostT cost( VertexT from, VertexT to) const override{
-        //map<EdgeT,CostT> :: iterator it;
-        EdgeT curEdge(from,to);//key
-        auto iter=costsMap.find(curEdge);
-        if(iter==costsMap.end()){//can't find
-            return infty;
-        }else{//find
-            return iter->second;//value
+    const NeighborT getNeighbors(VertexT v) const override {
+        return adjacencyList[v];
+    }
+
+    CostT cost(VertexT from, VertexT to) const override {
+        if (from == to) {
+            return 0;
         }
+        for (LocalEdgeT edge : adjacencyList[from]) {
+            if (edge.first == to) {
+                return edge.second;
+            }
+        }
+        return infty;
+    }
+
+    void resisze(int num_verts) {
+        vertexCount = num_verts;
+        adjacencyList.resize(num_verts);
+        coordinates.resize(num_verts);
+    }
+    
+    void addEdge(VertexT vertex, LocalEdgeT edge) {
+        adjacencyList[vertex].push_back(edge);
+    }
+
+    void addCoordinates(int vertex, pair<double, double> coord) {
+        coordinates[vertex] = coord;
     }
 
     void updateCost(VertexT from, VertexT to, CostT newCost) const{
         EdgeT curEdge(from,to);//key
         costsMap[curEdge]=newCost;//how to update the key-value pair in <map>?
     }
+
+    friend std::istream& operator>>(std::istream&, CoordinateGraph&);
 };
 
-class DisCoordGraph : public CoordinateGraph {
-    public:
-        CostT estimatedCost( VertexT from, VertexT to) const override{
+class EucliGraph : public CoordinateGraph {
+public:
+    EucliGraph(int num_verts = 0)
+        : CoordinateGraph(num_verts) {}
 
+    virtual ~EucliGraph() {}
+
+    double euclideanDistance(VertexT from, VertexT to) const {
+        double diffLat = coordinates[to].first - coordinates[from].first;
+        double diffLon = coordinates[to].second - coordinates[from].second;
+
+        double dist = sqrt(pow(diffLat, 2) + pow(diffLon, 2));
+
+        return dist;
+    }
+};
+
+class CircEucliGraph : public EucliGraph {
+public:
+    CircEucliGraph(int num_verts = 0)
+        : EucliGraph(num_verts) {}
+    
+    ~CircEucliGraph() {
+        for (size_t i = 0; i < adjacencyList.size(); i++) {
+            adjacencyList[i].clear();
+            adjacencyList[i].shrink_to_fit();
         }
-};
+        adjacencyList.clear();
+        adjacencyList.shrink_to_fit();
 
-class TimeCoordGraph : public CoordinateGraph{
-    public:
-        CostT estimatedCost( VertexT from, VertexT to) const override{
-
-        }
-};
-
-class MazeGraph : public DistanceGraph {
-    const NeighborT getNeighbors( VertexT v) const override{
-
+        coordinates.clear();
+        coordinates.shrink_to_fit();
     }
     
-    CostT estimatedCost( VertexT from, VertexT to) const override{
-
-    }
-    
-    CostT cost( VertexT from, VertexT to) const override{
-
+    CostT estimatedCost(VertexT from, VertexT to) const override {
+        double distance = min(euclideanDistance(from, to), min(cost(from, to), cost(to, from)));
+        return distance;
     }
 };
+
+class ShortDistEucliGraph : public EucliGraph {
+public:
+    ShortDistEucliGraph(int num_verts = 0)
+        : EucliGraph(num_verts) {}
+    
+    ~ShortDistEucliGraph() {
+        for (size_t i = 0; i < adjacencyList.size(); i++) {
+            adjacencyList[i].clear();
+            adjacencyList[i].shrink_to_fit();
+        }
+        adjacencyList.clear();
+        adjacencyList.shrink_to_fit();
+
+        coordinates.clear();
+        coordinates.shrink_to_fit();
+    }
+    
+    CostT estimatedCost(VertexT from, VertexT to) const override {
+        return euclideanDistance(from, to);
+    }
+};
+
+class LonLatCoordGraph : public CoordinateGraph {
+public:
+    LonLatCoordGraph(int num_verts = 0)
+        : CoordinateGraph(num_verts) {}
+    
+    virtual ~LonLatCoordGraph() {}
+
+    double latLongToDist(VertexT from, VertexT to) const {
+        double earthRadius = 6378.388;
+        double lon1 = coordinates[from].second * M_PI/180;
+        double lon2 = coordinates[to].second * M_PI/180;
+        double lat1 = coordinates[from].first * M_PI/180;
+        double lat2 = coordinates[to].first * M_PI/180;
+        double dist = earthRadius * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1));
+
+        return dist;
+    }
+};
+
+class LongDistCoordGraph : public LonLatCoordGraph {
+public:
+    LongDistCoordGraph(int num_verts = 0)
+        : LonLatCoordGraph(num_verts) {}
+    
+    ~LongDistCoordGraph() {
+        for (size_t i = 0; i < adjacencyList.size(); i++) {
+            adjacencyList[i].clear();
+            adjacencyList[i].shrink_to_fit();
+        }
+        adjacencyList.clear();
+        adjacencyList.shrink_to_fit();
+
+        coordinates.clear();
+        coordinates.shrink_to_fit();
+    }
+    
+    CostT estimatedCost(VertexT from, VertexT to) const override {
+        return latLongToDist(from, to);
+    }
+};
+
+class TimeCoordGraph : public LonLatCoordGraph {
+public:
+    TimeCoordGraph(int num_verts = 0)
+        : LonLatCoordGraph(num_verts) {}
+    
+    ~TimeCoordGraph() {
+        for (size_t i = 0; i < adjacencyList.size(); i++) {
+            adjacencyList[i].clear();
+            adjacencyList[i].shrink_to_fit();
+        }
+        adjacencyList.clear();
+        adjacencyList.shrink_to_fit();
+
+        coordinates.clear();
+        coordinates.shrink_to_fit();
+    }
+    
+    CostT estimatedCost( VertexT from, VertexT to) const override {
+        return (latLongToDist(from, to)/200.0)*60.0;
+    }
+};
+
+istream& operator>>(istream &s, CoordinateGraph &graph) {
+    int num_verts;
+    int num_edges;
+    VertexT vertex;
+    pair<VertexT, CostT> edge;
+    pair<double, double> coord;
+
+    s >> num_verts;
+
+    graph.resisze(num_verts);
+
+    s >> num_edges;
+
+    for (int i = 0; i < num_edges; i++){
+        s >> vertex;
+        s >> edge.first;
+        s >> edge.second;
+        graph.addEdge(vertex, edge);
+    }
+
+    for (int i = 0; i < num_verts; i++) {
+        s >> coord.first;
+        s >> coord.second;
+        graph.addCoordinates(i, coord);
+    }
+
+    return s;
+}
 
 void Dijkstra(const DistanceGraph& g, GraphVisualizer& v, VertexT start, std::vector<CostT>& kostenZumStart) {
     // ...
@@ -113,9 +265,50 @@ bool A_star(const DistanceGraph& g, GraphVisualizer& v, VertexT start, VertexT z
 int main()
 {
     // Frage Beispielnummer vom User ab
-    
     // Lade die zugehoerige Textdatei in einen Graphen
     // PruefeHeuristik
+    int example;
+    cout << "Geben Sie eine Beispielnummer zwischen 1 und 10 ein." << endl;
+    cin >> example;
+
+    if (example == 1) {
+        ShortDistEucliGraph graph;
+        ifstream s("Graph1.dat");
+        s >> graph;
+        PruefeHeuristik(graph);
+    } else if (example == 2) {
+        CircEucliGraph graph;
+        ifstream s("Graph2.dat");
+        s >> graph;
+        PruefeHeuristik(graph);
+    } else if (example == 3) {
+        LongDistCoordGraph graph;
+        ifstream s("Graph3.dat");
+        s >> graph;
+        PruefeHeuristik(graph);
+    } else if (example == 4) {
+        TimeCoordGraph graph;
+        ifstream s("Graph4.dat");
+        s >> graph;
+        PruefeHeuristik(graph);
+    } else if (example >= 5 && example <= 9) {
+        MazeGraph graph;
+        int num = example - 4;
+        string file = "Maze" + to_string(num) + ".dat";
+        ifstream s(file);
+        s >> graph;
+        PruefeHeuristik(graph);
+    } else if (example == 10) {
+        unsigned int seed;
+        cout << "Geben Sie einen seed ein." << endl;
+        cin >> seed;
+        vector<CellType> maze;
+        maze = ErzeugeLabyrinth(256, 256, seed);
+        MazeGraph graph(maze, 256, 256);
+    } else {
+        cout << "Ungültige Beispielnummer." << endl;
+        return -1;
+    }
     
     // Loese die in der Aufgabenstellung beschriebenen Probleme fuer die jeweilige Datei
     // PruefeDijkstra / PruefeWeg
